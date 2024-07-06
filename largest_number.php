87ce89d2ad18a88104b6367f9441798027392d27
Md. Abdul Kaioum <88127036+kaioumweb@users.noreply.github.com>
2024-07-06 12:50:09 +0600

    largest_number.php

--- largest_number.php
+++ largest_number.php
@@ -1,18 +1,17 @@
 <?php
-// Given numbers
-$num1 = 4;
-$num2 = 5;
-$num3 = 6;
+	//Set Value
+	$num1 = 4;
+	$num2 = 5;
+	$num3 = 6;
 
-// Finding the largest number using if-else statements
-if ($num1 >= $num2 && $num1 >= $num3) {
-    $largest = $num1;
-} elseif ($num2 >= $num1 && $num2 >= $num3) {
-    $largest = $num2;
-} else {
-    $largest = $num3;
-}
+	//largest number 
+	if($num1 >= $num2 && $num1 >= $num3){
+		$largest = $num1;
+	}elseif ($num2 >= $num1 && $num2 >= $num3) {
+		$largest = $num2;
+	}else {
+		$largest = $num3;
+	}
 
-// Print the largest number
-echo "The largest number among $num1, $num2, and $num3 is: $largest";
+	echo "largest number is: $largest";
 ?>
